bfcf5f52b11b545fa4485fc09f2438693ff3142f
dennismutitu <dennismutitu06@gmail.com>
2024-06-05 17:44:54 +0300

    strings in typescript

---
+++ String.ts
@@ -0,0 +1,5 @@
+// As in other languages, we use the type string to refer to these textual datatypes.
+// Just like JavaScript, TypeScript also uses double quotes (") or single quotes (') to surround string data.
+
+let color: string = "blue";
+color = 'red';
