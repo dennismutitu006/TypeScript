b471db096780c78e027fa37b39971eb086c54fa0
dennismutitu <dennismutitu06@gmail.com>
2024-06-05 17:43:22 +0300

    typescript numbers

---
+++ number.ts
@@ -0,0 +1,8 @@
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let big: bigint = 100n;
+//As in JavaScript, all numbers in TypeScript are either floating point values
+//These floating point numbers get the type number, while BigIntegers get the type bigint.
+//In addition to hexadecimal and decimal literals, TypeScript also supports binary and octal literals introduced in ECMAScript 2015.
