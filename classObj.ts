e06db39ce8269b6383323c6b33c6f99ef37419a9
dennismutitu <dennismutitu06@gmail.com>
2024-06-05 17:33:14 +0300

    classes and objects

---
+++ classObj.ts
@@ -0,0 +1,13 @@
+//There is no difference between how classes and objects conform to shapes:
+class VirtualPoint {
+  x: number;
+  y: number;
+ 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint); // logs "13, 56"
